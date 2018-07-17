a0852cda7e8e3f758e970742dc9b722b758cc412
Zachary_Theodore <fk9836@wayne.edu>
2018-07-17 15:10:08 -0400

    NO NEGATIVES

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -14,14 +14,18 @@ namespace Lab2
             string UserInput = Console.ReadLine();
             if (UserInput.ToLower() == "y")
             {
-                //fix this recursion
                 Main();
             }
             else
             {
-                //fix this hard stop
                 return;
             }
+        }
+
+        static void Restart()
+        {
+            Console.WriteLine("Sorry, But you have entered a negative Number, The program will restart now.");
+            Main();
         }
 
         static void Main()
@@ -36,42 +40,63 @@ namespace Lab2
             double widthInches;
             double heightFeet;
             double heightInches;
-            //add in ability to put in inches and feet and convert.
+            
             Console.WriteLine("Enter feet for the Length: ");
             lengthFeet = double.Parse(Console.ReadLine());
+            if (lengthFeet < 0)
+            {
+                Restart();
+            }
+
             Console.WriteLine("Enter inches for the Length: ");
             lengthInches = double.Parse(Console.ReadLine());
+            if (lengthInches < 0)
+            {
+                Restart();
+            }
 
             Console.WriteLine("Enter feet for the Width: ");
             widthFeet = double.Parse(Console.ReadLine());
+            if (widthFeet < 0)
+            {
+                Restart();
+            }
+
             Console.WriteLine("Enter inches for the Width: ");
             widthInches = double.Parse(Console.ReadLine());
+            if (widthInches < 0)
+            {
+                Restart();
+            }
 
             Console.WriteLine("Enter feet for the Height: ");
             heightFeet = double.Parse(Console.ReadLine());
+            if (heightFeet < 0)
+            {
+                Restart();
+            }
+
             Console.WriteLine("Enter inches for the Height: ");
             heightInches = double.Parse(Console.ReadLine());
-           
-            
+            if (heightInches < 0)
+            {
+                Restart();
+            }
+
             //solve problem
             double length = lengthFeet + (lengthInches / 12);
-            //Console.WriteLine(length);
             double width = widthFeet + (widthInches / 12);
-           // Console.WriteLine(width);
             double height = heightFeet + (heightInches / 12);
-           // Console.WriteLine(height);
 
             double Area = length * width;
-            //Console.WriteLine(Area);
             double Parimeter = 2 * length + 2 * width;
-            //Console.WriteLine(Parimeter);
             double Volume = length * width * height;
-            // Console.WriteLine(Volume);
+     
             int Area2 = Convert.ToInt32(Math.Floor(Area));
             int Parimeter2 = Convert.ToInt32(Math.Floor(Parimeter));
             int Volume2 = Convert.ToInt32(Math.Floor(Volume));
+           
             //Output
-
             Console.WriteLine($"Area: {Area - (Area2-Area)*-1}Feet, {(Area-Area2)*12}Inches.");
             Console.WriteLine($"Perimeter: {Parimeter - (Parimeter2-Parimeter)*-1}Feet, {(Parimeter-Parimeter2)*12}Inches.");
             Console.WriteLine($"Volume: {Volume - (Volume2-Volume)*-1}Feet, {(Volume-Volume2)*12}Inches.");
